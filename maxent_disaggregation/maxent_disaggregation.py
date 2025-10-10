a5e6e09849580669a8e76e99c57cad2c498969c2
Arthur Jakobs <arthur.jakobs@indecol.uni-freiburg.de>
2025-10-10 15:23:41 +0200

    checks on data input types and more descriptive error messages

--- maxent_disaggregation/maxent_disaggregation.py
+++ maxent_disaggregation/maxent_disaggregation.py
@@ -36,15 +36,15 @@ def maxent_disagg(
     mean_0:
         The best guess of the aggregate value.
     shares:
-        The best guesses for the shares. The sum of the shares should be 1 (unless there are NA's).
+        The best guesses for the shares. The sum of the shares should be 1 (unless there are NA's). Use np.nan for NA's.
     sd_0:
-        The standard deviation of the aggregate value.
+        The standard deviation of the aggregate value. Set to None or NA if not available.
     min:
         The lower boundary of the aggregate value.
     max:
         The upper boundary of the aggregate value.
     sds:
-        The standard deviations of the shares. Set to None if not available.
+        The standard deviations of the shares. Set to None if not available. Use np.nan for NA's.
     log:
         If True, the lognormal distribution is used for the aggregate value when a mean
         and a standard deviation are provided. If False, samples are drawn from a truncated
@@ -57,17 +57,18 @@ def maxent_disagg(
         A 2D array of shape (n, len(shares)) containing the generated samples.
     """
 
+
     # Check if shares and sds are numpy arrays or lists
-    if type(shares) != np.ndarray:
+    if  type(shares) != np.ndarray:
         if type(shares) == list:
             shares = np.array(shares)
         else:
-            raise ValueError("Shares should be a numpy array or a list.")
-    if type(sds) != np.ndarray and sds is not None:
+            raise ValueError("Shares should be a numpy array or a list. If no shares are known, set them them to np.nan")
+    if sds is not None and type(sds) != np.ndarray:
         if type(sds) == list:
             sds = np.array(sds)
         else:
-            raise ValueError("Sds should be a numpy array or a list.")
+            raise ValueError("Sds should be a numpy array or a list, or None.")
 
     # check shares sum to 1
     if not np.any(np.isnan(shares)):
@@ -81,6 +82,12 @@ def maxent_disagg(
     if sds is not None:
         if len(shares) != len(sds):
             raise ValueError("Shares and sds should have the same length.")
+
+    # Checks on sd_0
+    if sd_0 is not None and sd_0 < 0:
+        raise ValueError("sd_0 should be non-negative, or None/NA if not available.")
+    if sd_0 == 0:
+        raise ValueError("sd_0 should be positive, or None/NA if not available.")
 
     # The code below is only necessary if we want to accept arrays as input
     # if sd_0 is not None:
@@ -143,6 +150,10 @@ def sample_aggregate(
         Default is True
     """
 
+    # harmonize input of sd
+    if sd is not None and np.isnan(sd):
+        sd = None
+
     if (
         mean is not None
         and sd is not None
@@ -183,8 +194,12 @@ def sample_aggregate(
     ):
         # Uniform
         return np.random.uniform(low=low_bound, high=high_bound, size=n)
+    elif mean is not None and sd is None and low_bound not in [0, None]:
+        raise ValueError("Case with mean, no sd, and non-zero lower bound, or non-finite high bound is not implemented at the moment.")
+    elif mean is not None and sd is None and np.isfinite(high_bound):
+        raise ValueError("Case with mean, no sd, and non-zero lower bound, or non-finite high bound is not implemented at the moment.")
     else:
-        raise ValueError("Case not implemented atm.")
+        raise ValueError("Combination of inputs not implemented. Please check the input values.")
 
 
 def plot_samples_hist(
